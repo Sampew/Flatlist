cc33c6fcb20675b426161ba5c22bd1826df6a6d0
Sampo Ikonen <sampo.ikonen1@gmail.com>
2023-09-03 12:49:01 +0300

    Flatlist

---
+++ Data.js
@@ -0,0 +1,23 @@
+
+
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Vatanen',
+        firstname: 'PT'
+    },
+    {
+        id: '2',
+        lastname: 'Testonen',
+        firstname: 'Timo'
+    },
+    {
+        id:'3',
+        lastname: 'Järvi',
+        firstname: 'Suuri'
+    }
+]
+
+export default {
+    DATA
+}
